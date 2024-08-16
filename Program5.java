f5831e90e528ed11e898882dff99cb19808538f1
Morgan428 <MorganWare428@gmail.com>
2024-08-16 08:53:07 -0700

    Update Program5.java
    Needs to be tested

--- Program5.java
+++ Program5.java
@@ -10,6 +10,45 @@ public class Program5{
     System.out.println("Enter name of car:");
     Car c1 = new Car;
     c1.n = s.nextLine();
+    System.out.println("Enter miles for car:");
+    c1.mi = s.nextInt();
+    System.out.println("Enter gallons for car:");
+    c1.ga = s.nextInt();
+
+    System.out.println("Enter name of car:");
+    Car c2 = new Car;
+    c2.n = s.nextLine();
+    System.out.println("Enter miles for car:");
+    c2.mi = s.nextInt();
+    System.out.println("Enter gallons for car:");
+    c2.ga = s.nextInt();
+    
+    System.out.println("Enter name of car:");
+    Car c3 = new Car;
+    c3.n = s.nextLine();
+    System.out.println("Enter miles for car:");
+    c3.mi = s.nextInt();
+    System.out.println("Enter gallons for car:");
+    c3.ga = s.nextInt();
+    
+    System.out.println("Enter name of car:");
+    Car c4 = new Car;
+    c4.n = s.nextLine();
+    System.out.println("Enter miles for car:");
+    c4.mi = s.nextInt();
+    System.out.println("Enter gallons for car:");
+    c4.ga = s.nextInt();
+
+    System.out.println();
+    System.out.println("Mushroom Cup Prix Racer Average Miles/Per Gallon:");
+    System.out.print(c1.name);
+    System.out.println(" averaged " + c1.mi/c1.ga " m/g");
+    System.out.print(c2.name);
+    System.out.println(" averaged " + c2.mi/c2.ga " m/g");
+    System.out.print(c3.name);
+    System.out.println(" averaged " + c3.mi/c3.ga " m/g");
+    System.out.print(c4.name);
+    System.out.println(" averaged " + c4.mi/c4.ga " m/g");
   }
 }
 //Paste console output below:
