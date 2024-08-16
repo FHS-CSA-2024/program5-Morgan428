154a982d0b46e87f28662012cffb60d6e62e0ae3
Morgan428 <MorganWare428@gmail.com>
2024-08-16 14:25:24 -0700

    Update Program5.java
    finsing amth

--- Program5.java
+++ Program5.java
@@ -1,56 +1,46 @@
 import java.util.Scanner;
 public class Program5{
-    
-  public Car(String n, int mi, int ga){
-  }
   public static void main(String[] args){
     Scanner s = new Scanner(System.in);
     System.out.println("Enter name of car:");
-    Car c1 = new Car("n", 0, 0);
-    c1.n = s.nextLine();
+    String aN = s.nextLine();
     System.out.println("Enter miles for car:");
-    c1.mi = s.nextInt();
+    int aM = s.nextInt();
     System.out.println("Enter gallons for car:");
-    c1.ga = s.nextInt();
-
-    System.out.println("Enter name of car:");
-    Car c2 = new Car("n", 0, 0);
-    c2.n = s.nextLine();
-    System.out.println("Enter miles for car:");
-    c2.mi = s.nextInt();
-    System.out.println("Enter gallons for car:");
-    c2.ga = s.nextInt();
+    int aG = s.nextInt();
     
     System.out.println("Enter name of car:");
-    Car c3 = new Car("n", 0, 0);
-    c3.n = s.nextLine();
+    String bN = s.next();
     System.out.println("Enter miles for car:");
-    c3.mi = s.nextInt();
+    int bM = s.nextInt();
     System.out.println("Enter gallons for car:");
-    c3.ga = s.nextInt();
+    int bG = s.nextInt();
     
     System.out.println("Enter name of car:");
-    Car c4 = new Car("n", 0, 0);
-    c4.n = s.nextLine();
+    String cN = s.next();
     System.out.println("Enter miles for car:");
-    c4.mi = s.nextInt();
+    int cM = s.nextInt();
     System.out.println("Enter gallons for car:");
-    c4.ga = s.nextInt();
-
-    System.out.println();
+    int cG = s.nextInt();
+    
+    System.out.println("Enter name of car:");
+    String dN = s.next();
+    System.out.println("Enter miles for car:");
+    int dM = s.nextInt();
+    System.out.println("Enter gallons for car:");
+    int dG = s.nextInt();
+    
     System.out.println("Mushroom Cup Prix Racer Average Miles/Per Gallon:");
-    System.out.print(c1.name);
-    System.out.println(" averaged " + c1.mi/c1.ga + " m/g");
-    System.out.print(c2.name);
-    System.out.println(" averaged " + c2.mi/c2.ga + " m/g");
-    System.out.print(c3.name);
-    System.out.println(" averaged " + c3.mi/c3.ga + " m/g");
-    System.out.print(c4.name);
-    System.out.println(" averaged " + c4.mi/c4.ga + " m/g");
+    double a = aM / aG;
+    System.out.println(aN + " averaged " + a + " m//g");
+    double b = bM / bG;
+    System.out.println(bN + " averaged " + b + " m//g");
+    double c cM / cG;
+    System.out.println(cN + " averaged"  + a + " m//g");
+    System.out.println(dN + " averaged " + a + " m//g");
   }
 }
 //Paste console output below:
 /*
-
 
 */
