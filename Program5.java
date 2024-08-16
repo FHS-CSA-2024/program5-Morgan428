34f643419607e9b14fa0f7d3e2090dd886d1b8cc
Morgan428 <MorganWare428@gmail.com>
2024-08-16 08:44:11 -0700

    Update Program5.java
    Started taking typed data

--- Program5.java
+++ Program5.java
@@ -1,7 +1,17 @@
-//import stuff here?
-
-//Your code here
-
+import.java.until.Scanner;
+public class Program5{
+  public class Car(String n, int mi, int ga){
+    String n;
+    int mi = 0;
+    int ga = 0;
+  }
+  public static void main(String[] args){
+    Scanner s = new Scanner(system.in);
+    System.out.println("Enter name of car:");
+    Car c1 = new Car;
+    c1.n = s.nextLine();
+  }
+}
 //Paste console output below:
 /*
 
